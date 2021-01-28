bf8d415bf27fb65ba01028e2abaa9b739b238192
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-28 12:45:02 -0500

    add a custom HitCounter - counts number of clicks

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <Counter />
   <HoverLabel />
+  <HitCounter />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Counter from './components/Counter.vue';
 import HoverLabel from './components/HoverLabel.vue';
+import HitCounter from './components/HitCounter.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     Counter,
-    HoverLabel
+    HoverLabel,
+    HitCounter
   }
 }
 </script>
---
+++ src/components/HitCounter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="hit-counter" class="demo">
+    <p>Counter: {{ counter }}</p>
+    <button v-on:click="increaseCount">Increase Count</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HitCounter",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increaseCount() {
+      this.counter = this.counter + 1;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
